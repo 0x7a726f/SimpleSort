5e1f93bf474ad85eaebd143de8cc3fb9226314ad
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2022-11-09 23:54:20 +0200

    Create sort2.py

---
+++ sort2.py
@@ -0,0 +1,34 @@
+import time
+
+def f(n):
+            sortingList : list = []
+
+            with open(n, "r") as bef:
+                numbers = str(bef.read())
+                sortingList = [int(x) for x in numbers.split(",")]
+            bef.close()
+
+            global start_time 
+            start_time = time.time()
+
+            for _ in range(len(sortingList)-1):
+                for i in range(len(sortingList)-1):
+                    if sortingList[i] > sortingList[i+1]:
+                        sortingList[i+1],sortingList[i]=sortingList[i],sortingList[i+1]
+            
+            print("Time to sort :" ,str(time.time() - start_time))
+
+            with open("sorted.txt", "x") as aft:
+                for i in range(len(sortingList)):
+                    aft.write(str(sortingList[i]))
+                    aft.write(",")
+            aft.close()
+
+def main():
+    try:
+        f(str(input("Enter txt file location >>")))
+        main()
+    except:
+        print("Error!")
+        main()
+main()
